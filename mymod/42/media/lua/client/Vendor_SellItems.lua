64493a44236a4c307be1efbf26975ff23e4dc98a
FGCFlukey <tomvalenz@hotmail.com>
2026-02-13 21:20:01 -0500

    UI improvements for Vendor
    Fixed up the UI and item list files so now we have tabbed categories, Sell tab will list all items you can sell to vendor.
    Fixed up the UI rendering, making the window bigger and less hassle

--- mymod/42/media/lua/client/Vendor_SellItems.lua
+++ mymod/42/media/lua/client/Vendor_SellItems.lua
@@ -1,17 +1,18 @@
 -- Vendor_SellItems.lua
 -- Items the player can SELL to the Emergency Vendor
+-- Clean format (no category headers needed)
 
 VendorSellItems = {
 
-    { category = true, name = "-- Sellable Gems & Crystal --" },
+    -- Gems
+    { id = "Base.Diamond",        name = "Diamond",        price = 200 },
+    { id = "Base.Emerald",        name = "Emerald",        price = 150 },
+    { id = "Base.Ruby",           name = "Ruby",           price = 175 },
+    { id = "Base.Sapphire",       name = "Sapphire",       price = 120 },
+    { id = "Base.Amethyst",       name = "Amethyst",       price = 100 },
 
-    { id = "Base.Diamond",              name = "Diamond",                       price = 200 },
-    { id = "Base.Emerald",              name = "Emerald",                       price = 150 },
-    { id = "Base.Ruby",                 name = "Ruby",                          price = 175 },
-    { id = "Base.Sapphire",             name = "Sapphire",                      price = 120 },
-    { id = "Base.Amethyst",             name = "Amethyst",                      price = 100 },
-    
-    { id = "Base.Crystal_Large",        name = "Large Crystal",                 price = 60 },
-    { id = "Base.Crystal",              name = "Crystal",                       price = 40 },
+    -- Crystals
+    { id = "Base.Crystal_Large",  name = "Large Crystal",  price = 60 },
+    { id = "Base.Crystal",        name = "Crystal",        price = 40 },
 
 }
